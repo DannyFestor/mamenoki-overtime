6ce28f69e74a5933fe49dff627d702d97efc0c54
Danny Festor <danny@festor.info>
2023-12-14 14:32:54 +0900

    Add Overtime Create Button

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -11,7 +11,7 @@
             </div>
         </div>
         <div>
-            <a href="{{ route('overtime.index') }}" class="h-64 w-64 rounded-full border-8 border-emerald-300 bg-emerald-100 text-emerald-800 flex justify-center items-center text-center text-2xl transition-all hover:scale-105">
+            <a wire:navigate href="{{ route('overtime.index') }}" class="h-64 w-64 rounded-full border-8 border-emerald-300 bg-emerald-100 text-emerald-800 flex justify-center items-center text-center text-2xl transition-all hover:scale-105">
                 時間外勤務<br>
                 休日出勤<br>
                 申請
--- resources/views/livewire/overtime/index.blade.php
+++ resources/views/livewire/overtime/index.blade.php
@@ -1,23 +1,26 @@
 <div class="w-full max-w-7xl mx-auto flex flex-col gap-4">
     <div class="w-full flex justify-between border-2 border-sky-900 p-4">
         <div>{{ $name }}</div>
+
         <div class="flex gap-2">
             <x-svgs.chevron-left wire:click="decreaseYear" class="cursor-pointer" />
             {{ $this->japaneseYear() }}年度 時間外勤務命令簿
             <x-svgs.chevron-right wire:click="increaseYear" class="cursor-pointer" />
         </div>
+
         <div class="flex gap-2">
             <x-svgs.chevron-left wire:click="decreaseMonth" class="cursor-pointer" />
             {{ $month }}月分
             <x-svgs.chevron-right wire:click="increaseMonth" class="cursor-pointer" />
         </div>
     </div>
-    <div class="w-full flex">
+
+    <div class="w-full flex gap-4">
         <div class="flex flex-col border border-2 border-sky-900">
             @if(!$this->hasCurrentYear())
                 <a wire:navigate
                    href="{{ route('overtime.index', ['year' => now()->year, 'month' => now()->month]) }}"
-                   class="px-4 py-2 hover:bg-sky-200"
+                   class="px-4 py-2 hover:bg-sky-100"
                 >
                     {{ now()->year }}年{{ str_pad(now()->month, 2, '0', STR_PAD_LEFT) }}月
                 </a>
@@ -30,6 +33,12 @@
                     {{ $overtimeConfirmation['year'] }}年{{ str_pad($overtimeConfirmation['month'], 2, '0', STR_PAD_LEFT) }}月
                 </a>
             @endforeach
+        </div>
+
+        <div class="flex-1 flex flex-col gap-4">
+            <a wire:navigate href="{{ route('overtime.create') }}" class="w-full border-2 border-sky-900 px-4 py-2 text-xl flex justify-center items-center gap-2 hover:bg-sky-100">
+                新規作成 <x-svgs.plus-circle />
+            </a>
         </div>
     </div>
 </div>
