4b1fe8f7bed9b1b7659826fd121024e2680d1f25
Danny Festor <danny@festor.info>
2023-12-14 11:30:09 +0900

    Make Dashboard Livewire

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', \App\Livewire\Dashboard::class)->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/test', \App\Livewire\Test::class);
 
 require __DIR__.'/auth.php';
