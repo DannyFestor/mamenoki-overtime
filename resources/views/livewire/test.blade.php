59d188b4d02af3d13635d8bb734d9aad98b44adb
Danny Festor <danny@festor.info>
2023-12-14 11:26:48 +0900

    Add Livewire

---
+++ resources/views/livewire/test.blade.php
@@ -0,0 +1,3 @@
+<div class="text-red-500 text-xl font-bold">
+    Haro from Livewire
+</div>
--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/test', \App\Livewire\Test::class);
+
 require __DIR__.'/auth.php';
